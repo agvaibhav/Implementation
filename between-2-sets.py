e5bb6ea7d408f81ff857a90d0cf8ae64447ffc8c
Vaibhav Agarwal <42164841+agvaibhav@users.noreply.github.com>
2018-09-12 14:47:31 +0530

    Update between-2-sets.py
    using lcm and hcf function 
    but takes long time to run

--- between-2-sets.py
+++ between-2-sets.py
@@ -10,44 +10,41 @@ def getTotalX(a, b):
     #
     # Write your code here.
     #
-    maxa = max(a)
-    minb = min(b)
-    maxaarr=[]
-    arra=[]
-    i=1
-    try:
-        while(maxa*i<=minb):
-            maxaarr.append(maxa*i)
-            i+=1
-        for k in a:
-            j=0
-            while(maxaarr[j]!=maxaarr[-1]):
-                if maxaarr[j]%k==0:
-                    arra.append(maxaarr[j])
-                    j+=1
-                else:
-                    if maxaarr[j] in arra:
-                        arra.remove(maxaarr[j])
-                    maxaarr.remove(maxaarr[j])
-            if maxaarr[j]%k==0:
-                    arra.append(maxaarr[j])
-            else:
-                    maxaarr.remove(maxaarr[j])
-                    if maxaarr[j] in arra:
-                        arra.remove(maxaarr[j])
-            arra = list(set(arra))
-        res=[]
-        for elea in arr:
-            count =0
-            for eleb in b:
-                if eleb%elea == 0:
-                    count+=1
-            if count == len(b):
-                res.append(elea)
-        return len(res)
-    except:
-        return 0
-        
+    def lcm(x,y):
+        if a>b:
+            z = x
+        elif x<y:
+            z = y
+        for i in range(z, x*y+1):
+            if i%x==0 and i%y==0:
+                return i
+            
+    def hcf(x,y):
+        if x<y:
+            z=x
+        elif x>y:
+            z=y
+        for j in range(z,1,-1):
+            if x%j==0 and y%j==0:
+                return j
+    
+    for k in range(len(a)-1):
+        lcma=lcm(a[k],a[k+1])
+        a[k+1]=lcma
+
+    for l in range(len(b)-1):
+        hcfb = hcf(b[l],b[l+1])
+        b[k+1]=hcfb
+    
+    count = 0
+    c=1
+    while(lcma*c<=hcfb):
+        m=lcma*c
+        if hcfb%m==0:
+            count+=1
+        c+=1
+    return count
+
 if __name__ == '__main__':
     f = open(os.environ['OUTPUT_PATH'], 'w')
 
